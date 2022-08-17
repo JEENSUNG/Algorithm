341ddb70b55c7d07128dcd6ce98e6b41531972e8
Jeen Sung <61839458+JEENSUNG@users.noreply.github.com>
2022-08-18 02:46:07 +0900

    [level 2] Title: 올바른 괄호, Time: 18.75 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == ')'){
+                if(stack.isEmpty())
+                    return false;
+                stack.pop();
+            }else
+                stack.push('(');
+        }
+        return stack.size() == 0;
+    }
+}
