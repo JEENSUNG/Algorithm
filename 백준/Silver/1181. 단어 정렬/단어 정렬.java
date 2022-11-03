8b0469b374036f643f00f209e7a943a365635c53
Jeen Sung <61839458+JEENSUNG@users.noreply.github.com>
2022-11-03 18:32:30 +0900

    [Silver V] Title: 단어 정렬, Time: 836 ms, Memory: 40268 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Words{
+    int length;
+    String str;
+    Words(int length, String str){
+        this.length = length;
+        this.str = str;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        ArrayList<Words> list = new ArrayList<>();
+        HashSet<String> hashSet = new HashSet<>();
+        for(int i = 0; i < n; i++) {
+            String st = scan.next();
+            if(!hashSet.contains(st)) {
+                hashSet.add(st);
+                list.add(new Words(st.length(), st));
+            }
+        }
+        Collections.sort(list, new Comparator<Words>() {
+            @Override
+            public int compare(Words o1, Words o2) {
+                if(o1.length == o2.length)
+                    return o1.str.compareTo(o2.str);
+                return o1.length - o2.length;
+            }
+        });
+        for(int i = 0; i < list.size(); i++)
+            System.out.println(list.get(i).str);
+    }
+}
