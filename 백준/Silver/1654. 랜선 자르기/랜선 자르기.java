37ca4b7c9134e0ded3f8265da75544ecd89fb773
Jeen Sung <61839458+JEENSUNG@users.noreply.github.com>
2022-11-03 19:30:05 +0900

    [Silver II] Title: 랜선 자르기, Time: 532 ms, Memory: 31688 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Words{
+    int length;
+    String str;
+    Words(int length, String str){
+        this.length = length;
+        this.str = str;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int k = scan.nextInt();
+        int n = scan.nextInt();
+        ArrayList<Long> list = new ArrayList<>();
+        for(int i = 0; i < k; i++)
+            list.add(scan.nextLong());
+        Collections.sort(list);
+        long max = list.get(list.size() - 1);
+        long answer = 0;
+        long left = 1;
+        long right = (long)Integer.MAX_VALUE + 1;
+        long mid = 0;
+        int count = 0;
+        while (left < right){
+            count = 0;
+            mid = (left + right) / 2;
+            for(int i = 0; i < list.size(); i++)
+                count += list.get(i)/mid;
+            if(count >= n){
+                left = mid + 1;
+                answer = Math.max(answer, mid);
+            }else{
+                right = mid;
+            }
+        }
+        System.out.println(answer);
+    }
+}
