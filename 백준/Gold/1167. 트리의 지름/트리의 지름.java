087bb8a659253429f3e099cc359e8a071554c18c
Jeen Sung <61839458+JEENSUNG@users.noreply.github.com>
2022-11-10 23:25:42 +0900

    [Gold II] Title: 트리의 지름, Time: 1648 ms, Memory: 319268 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -10,18 +10,17 @@ class No{
 class Main{
 	static int v;
 	static int answer = 0;
-	static int[] arr;
-	static boolean[] visit;
-	static PriorityQueue<No> queue;
 	static List<No>[] list;
+	static int[] ans;
+	static boolean[] visit;
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		v = scan.nextInt();
 		list = new List[v + 1];
+		ans = new int[v + 1];
+		visit = new boolean[v + 1];
 		for(int i = 1; i <= v; i++)
 			list[i] = new ArrayList<No>();
-		arr = new int[v + 1];
-		visit = new boolean[v + 1];
 		int t = v;
 		while(t--> 0) {
 			int x = scan.nextInt();
@@ -32,34 +31,29 @@ class Main{
 				list[x].add(new No(a, b));
 			}
 		}
+		dfs(1);
+        int b = 0;
+        for(int i = 1; i <= v; i++){
+            if(answer < ans[i]){
+                answer = ans[i];
+                b = i;
+            }
+        }
+        answer = 0;
+         visit = new boolean[v + 1];
+        ans = new int[v + 1];
+        dfs(b);
 		for(int i = 1; i <= v; i++)
-			if(!visit[i])
-				bfs(i);
-		visit = new boolean[v + 1];
-		int b = 0;
-		for(int i = 1; i <= v; i++) {
-			if(answer < arr[i]) {
-				answer = arr[i];
-				b = i;
-			}
-		}
-		bfs(b);
-		for(int i = 1; i <= v; i++)
-			answer = Math.max(answer, arr[i]);
+			answer = Math.max(answer, ans[i]);
 		System.out.print(answer);
 	}
-	static void bfs(int x) {
-		arr = new int[v + 1];
-		queue = new PriorityQueue<No>((o1, o2) -> o2.dir - o1.dir);
-		queue.offer(new No(x, 0));
-		visit[x] = true;
-		while(!queue.isEmpty()) {
-			No now = queue.poll();
-			for(No next : list[now.x]) {
-				if(!visit[next.x]) {
-					arr[next.x] = now.dir + next.dir;
-					queue.offer(new No(next.x, arr[next.x]));
-					visit[next.x] = true; 
+	static void dfs(int x) {
+		if(!visit[x]) {
+			visit[x] = true;
+			for(No next : list[x]) {
+				if(!visit[next.x] && ans[next.x] < ans[x] + next.dir) {
+					ans[next.x] = ans[x] + next.dir;
+					dfs(next.x);
 				}
 			}
 		}
