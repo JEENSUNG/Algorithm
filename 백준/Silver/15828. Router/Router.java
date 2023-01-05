7f23688b775054df181a9fd1b106b05b634adc49
Jeen Sung <61839458+JEENSUNG@users.noreply.github.com>
2023-01-05 16:21:43 +0900

    [Silver IV] Title: Router, Time: 1820 ms, Memory: 187128 KB -BaekjoonHub

---
+++ 백준/Silver/15828. Router/Router.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		Queue<Integer> queue = new LinkedList<Integer>();
+		while(true) {
+			int x = scan.nextInt();
+			if(x == -1)
+				break;
+			if(x > 0) {
+				if(queue.size() < n)
+					queue.offer(x);
+			}
+			else if(x == 0)
+				if(!queue.isEmpty())
+					queue.poll();
+		}
+		while(!queue.isEmpty())
+			System.out.print(queue.poll() + " ");
+	}
+
+}
