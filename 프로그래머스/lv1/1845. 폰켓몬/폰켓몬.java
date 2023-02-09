391613883f46c0909e3e0b80e124ae43dd27a386
Jeen Sung <61839458+JEENSUNG@users.noreply.github.com>
2023-02-09 15:10:41 +0900

    [level 1] Title: 폰켓몬, Time: 0.42 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        boolean[] visit = new boolean[200001];
+        int count = 0;
+        for(int i = 0; i < nums.length; i++)
+            if(!visit[nums[i]]){
+                visit[nums[i]] = true;
+                count++;
+            }
+        return nums.length / 2 > count ? count : nums.length / 2;
+    }
+}
