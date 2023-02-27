a32a1cabc312d5186182e2e68fd982580af5cc47
Heo Jeen Sung <61839458+JEENSUNG@users.noreply.github.com>
2023-02-27 20:31:28 +0900

    [level 3] Title: 스타 수열, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv3/70130. 스타 수열/스타 수열.java
+++ 프로그래머스/lv3/70130. 스타 수열/스타 수열.java
@@ -2,19 +2,19 @@ class Solution {
     public int solution(int[] a) {
         int[] count = new int[a.length];
         for(int i : a)
-        	count[i]++;
+            count[i]++;
         int answer = 0;
         for(int i = 0; i < a.length; i++) {
-        	if(count[i] == 0) continue;
-        	if(count[i] < answer) continue;
-        	int size = 0;
-        	for(int j = 0; j < a.length-  1; j++) {
-        		if(a[j] != i && a[j + 1] != i) continue;
-        		if(a[j] == a[j + 1]) continue;
-        		size++;
-        		j++;
-        	}
-        	answer = Math.max(size,  answer);
+            if(count[i] == 0) continue;
+            if(count[i] < answer) continue;
+            int size = 0;
+            for(int j = 0; j < a.length-  1; j++) {
+                if(a[j] != i && a[j + 1] != i) continue;
+                if(a[j] == a[j + 1]) continue;
+                size++;
+                j++;
+            }
+            answer = Math.max(size,  answer);
         }
         return answer * 2;
     }
