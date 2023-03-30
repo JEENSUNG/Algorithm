62c92bf3dac09eeed46622ba449c9b006d4233b0
Heo Jeen Sung <61839458+JEENSUNG@users.noreply.github.com>
2023-03-30 21:03:21 +0900

    [Silver III] Title: 파일 정리, Time: 1752 ms, Memory: 83576 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+	static int n;
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextInt();
+		HashMap<String, Integer> hash = new HashMap<>();
+		while(n --> 0) {
+			String str = scan.next();
+			String before = "", after = "";
+			for(int i = 0; i < str.length(); i++)
+				if(str.charAt(i) == '.') {
+					before = str.substring(0, i);
+					after = str.substring(i + 1);
+					break;
+				}
+			hash.put(after, hash.getOrDefault(after, 0) + 1);
+		}
+		List<String> list = new ArrayList<>(hash.keySet());
+		Collections.sort(list);
+		for(String now : list)
+			System.out.println(now + " " +hash.get(now));
+	}
+
+}
