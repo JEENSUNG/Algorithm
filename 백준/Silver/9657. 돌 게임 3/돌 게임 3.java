5823d89395ac8f9c494461a5f79f259364ece67b
Heo Jeen Sung <61839458+JEENSUNG@users.noreply.github.com>
2023-04-06 21:20:56 +0900

    [Silver III] Title: 돌 게임 3, Time: 212 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		boolean[] dp = new boolean[1001];
+		dp[1] = dp[3] = dp[4] = true;
+		for(int i = 5; i <= n; i++) {
+			if(dp[i - 1] && dp[i - 3] && dp[i - 4])
+				dp[i] = false;
+			else
+				dp[i] = true;
+		}
+		if(dp[n])
+			System.out.println("SK");
+		else
+			System.out.println("CY");
+	}
+}
