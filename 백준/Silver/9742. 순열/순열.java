85e25eaf51c2e51cd83b15729eb2f63163f30067
Heo Jeen Sung <61839458+JEENSUNG@users.noreply.github.com>
2023-06-28 21:54:33 +0900

    [Silver III] Title: 순열, Time: 344 ms, Memory: 16952 KB -BaekjoonHub

--- 백준/Silver/9742. 순열/순열.java
+++ 백준/Silver/9742. 순열/순열.java
@@ -1,58 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class Main {
-	private static int N;
-	private static String arr[];
+import java.util.*;
+public class Main { 
+	static int n;
+	static String[] arr;
+	static boolean[] visit;
+	static int time;
+	static String[] ans;
 	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		String line=null;
-		while((line=br.readLine())!=null) {
-			StringTokenizer st=new StringTokenizer(line," ");
-			String temp=st.nextToken();
-			arr=temp.split("");
-			time=0;
-			temparr=new String[arr.length];
-			visit=new boolean[arr.length];
-			N=Integer.parseInt(st.nextToken());
-			find(0);
-			
-			System.out.print(temp+" "+N+" = ");
-			if(time<N) {	//순열이 없을 경우
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = null;
+		while((s = br.readLine()) != null) {
+			StringTokenizer st = new StringTokenizer(s, " ");
+			String str = st.nextToken();
+			arr = str.split("");
+			visit = new boolean[arr.length];
+			ans = new String[arr.length];
+			time = 0;
+			n = Integer.parseInt(st.nextToken());
+			dfs(0);
+			System.out.print(str + " " + n + " = ");
+			if(time < n)
 				System.out.println("No permutation");
-			}else {	//순열이 있을 경우
-				
-				for (int j = 0; j < arr.length; j++) {
-					System.out.print(arr[j]);
-				}
+			else {
+				for(int i = 0; i < arr.length; i++)
+					System.out.print(arr[i]);
 				System.out.println();
 			}
-			
 		}
 	}
-	private static String temparr[];
-	private static boolean visit[];
-	private static int time;
-	private static void find(int cnt) {
-		// TODO Auto-generated method stub
-		if(cnt==arr.length) { //순열이 만들어 졌을 경우
-			time++;	//순열의 수를 세어준다.
-			if(time==N) {	//해당 위치랑 같다면 결과 배열에 저장을 해준다.
-				for (int i = 0; i < arr.length; i++) {
-					arr[i]=temparr[i];
-				}
-				
-			}
+	static void dfs(int count) {
+		if(count == arr.length) {
+			time++;
+			if(time == n)
+				for(int i = 0; i < arr.length; i++)
+					arr[i] = ans[i];
 			return;
 		}
-		for (int i = 0; i < arr.length; i++) {	// 순열을 만들어 주는 작업이다.
-			if(visit[i])continue;	//visit 배열이 true면 무시한다.
-			temparr[cnt]=arr[i];	//임시배열에 순열을 저장해 준다.
-			visit[i]=true;
-			find(cnt+1);
-			visit[i]=false;
+		for(int i = 0; i < arr.length; i++) {
+			if(!visit[i]) {
+				ans[count] = arr[i];
+				visit[i] = true;
+				dfs(count + 1);
+				visit[i] = false;
+			}
 		}
 	}
 }
